a1b316ed2bec40bea300df62863b580a36028165
Venomalia <87765258+Venomalia@users.noreply.github.com>
2024-02-12 03:44:12 +0100

    add LZOn compression algorithm

---
+++ src/AuroraLib.Compression/Algorithms/LZOn.cs
@@ -0,0 +1,52 @@
+﻿using AuroraLib.Compression.Exceptions;
+using AuroraLib.Compression.Interfaces;
+
+namespace AuroraLib.Compression.Algorithms
+{
+    /// <summary>
+    /// LZO Nintendo mainly used in DS Download Games.
+    /// </summary>
+    public sealed class LZOn : ICompressionAlgorithm, ILzSettings, IHasIdentifier
+    {
+        /// <inheritdoc/>
+        public IIdentifier Identifier => _identifier;
+
+        private static readonly Identifier64 _identifier = new(new("LZOn"), new(0x00, 0x2F, 0xF1, 0x71));
+
+        /// <inheritdoc/>
+        public bool LookAhead { get; set; } = true;
+
+        /// <inheritdoc/>
+        public bool IsMatch(Stream stream, ReadOnlySpan<char> extension = default)
+            => stream.Position + 0x10 < stream.Length && stream.Match(_identifier);
+
+        /// <inheritdoc/>
+        public void Decompress(Stream source, Stream destination)
+        {
+            long destinationStartPosition = destination.Position;
+            source.MatchThrow(_identifier);
+            uint uncompressedSize = source.ReadUInt32(Endian.Big);
+            uint compressedSize = source.ReadUInt32(Endian.Big);
+            LZO.DecompressHeaderless(source, destination);
+
+            if (destination.Position - destinationStartPosition > uncompressedSize)
+            {
+                throw new DecompressedSizeException(uncompressedSize, destination.Position - destinationStartPosition);
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Compress(ReadOnlySpan<byte> source, Stream destination, CompressionLevel level = CompressionLevel.Optimal)
+        {
+            long destinationStartPosition = destination.Position;
+            destination.Write(_identifier);
+            destination.Write(source.Length, Endian.Big);
+            destination.Write(0); // Compressed length (will be filled in later)
+            LZO.CompressHeaderless(source, destination, LookAhead, level);
+
+            // Go back to the beginning of the file and write out the compressed length
+            int destinationLength = (int)(destination.Position - destinationStartPosition - 0x10);
+            destination.At(destinationStartPosition + 12, x => x.Write(destinationLength));
+        }
+    }
+}
