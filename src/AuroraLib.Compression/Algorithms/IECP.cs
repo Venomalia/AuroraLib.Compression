b8681e1e0407c2083bbb1af77920d343e345a82a
Venomalia <87765258+Venomalia@users.noreply.github.com>
2024-09-16 07:32:26 +0200

    add IECP algorithm from Fate/Extra

---
+++ src/AuroraLib.Compression/Algorithms/IECP.cs
@@ -0,0 +1,50 @@
+﻿using AuroraLib.Compression.Interfaces;
+using AuroraLib.Core;
+using AuroraLib.Core.Interfaces;
+using AuroraLib.Core.IO;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace AuroraLib.Compression.Algorithms
+{
+    /// <summary>
+    /// IECP algorithm base on LZSS, used in Fate/Extra.
+    /// </summary>
+    public class IECP : ICompressionAlgorithm, ILzSettings, IHasIdentifier
+    {
+        /// <inheritdoc/>
+        public IIdentifier Identifier => _identifier;
+
+        private static readonly Identifier32 _identifier = new Identifier32("IECP".AsSpan());
+
+        /// <inheritdoc/>
+        public bool LookAhead { get; set; } = true;
+
+        private static readonly LzProperties _lz = new LzProperties(0x1000, 0xF + 3, 3, 0xFEE);
+
+        /// <inheritdoc/>
+        public bool IsMatch(Stream stream, ReadOnlySpan<char> extension = default)
+            => IsMatchStatic(stream, extension);
+
+        /// <inheritdoc cref="IsMatch(Stream, ReadOnlySpan{char})"/>
+        public static bool IsMatchStatic(Stream stream, ReadOnlySpan<char> extension = default)
+            => stream.Position + 0x10 < stream.Length && stream.Match(_identifier);
+
+        /// <inheritdoc/>
+        public void Decompress(Stream source, Stream destination)
+        {
+            source.MatchThrow(_identifier);
+            uint decompressedSize = source.ReadUInt32();
+            LZSS.DecompressHeaderless(source, destination, (int)decompressedSize, _lz);
+        }
+
+        /// <inheritdoc/>
+        public void Compress(ReadOnlySpan<byte> source, Stream destination, CompressionLevel level = CompressionLevel.Optimal)
+        {
+            destination.Write(_identifier);
+            destination.Write(source.Length);
+            LZSS.CompressHeaderless(source, destination, _lz, LookAhead, level);
+        }
+    }
+}
