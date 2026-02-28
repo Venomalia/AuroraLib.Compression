ab75e97bf2a4875ee648927bdfea5a2a3e9c772b
Venomalia <87765258+Venomalia@users.noreply.github.com>
2026-02-28 17:45:47 +0100

    Improve ILzSettings LookAhead documentation

--- src/AuroraLib.Compression/Interfaces/ILzSettings.cs
+++ src/AuroraLib.Compression/Interfaces/ILzSettings.cs
@@ -1,13 +1,15 @@
 namespace AuroraLib.Compression.Interfaces
 {
     /// <summary>
-    /// Extends the <see cref="ICompressionEncoder"/> interface to provide functionality more settings for LZ-based compression algorithms, allowing for customization of compression behavior.
+    /// Extends the <see cref="ICompressionEncoder"/> interface with additional settings for LZ-based compression algorithms, allowing customization of compression behavior.
     /// </summary>
     public interface ILzSettings : ICompressionEncoder
     {
         /// <summary>
-        /// Enables the algorithm to analyze repeating patterns more effectively, resulting in smaller compressed files.
-        /// <para>Note: Activating this setting may cause issues if the specific implementation of a game does not support LookAhead optimization.</para>
+        /// Enables look-ahead matching, allowing the encoder to find longer and more optimal repeating patterns.
+        /// <para>
+        /// If enabled, this will improve the compression ratio, but may reduce compatibility with decoders that do not support look-ahead optimization.
+        /// </para>
         /// </summary>
         bool LookAhead { get; set; }
     }
